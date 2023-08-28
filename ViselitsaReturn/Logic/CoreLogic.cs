d35e22871c2977f0bc0eb11ec630cff587c300c0
risovanienasaite@gmail.com <risovanienasaite@gmail.com>
2023-08-28 21:11:12 +0300

    I did nothing, What a shitty try

---
+++ ViselitsaReturn/Logic/CoreLogic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViselitsaReturn.Logic
+{
+    internal class CoreLogic
+    {
+        List<string> list_Of_Word = new List<string>();
+
+        string word_To_Guess="";
+
+        CoreLogic()
+        {
+            list_Of_Word.Add("шляпа");
+            list_Of_Word.Add("мусор");
+            list_Of_Word.Add("дурка");
+            list_Of_Word.Add("машина");
+            list_Of_Word.Add("квартира");
+        }
+
+        void Pic_Random_Word(Random rnd, List<string> list)
+        {
+            string word_To_Guess = list[rnd.Next(0,list.Count-1)];
+        }
+
+
+
+    }
+}
--- ViselitsaReturn/MainWindow.xaml.cs
+++ ViselitsaReturn/MainWindow.xaml.cs
@@ -20,9 +20,43 @@ namespace ViselitsaReturn
     /// </summary>
     public partial class MainWindow : Window
     {
+        char s;
         public MainWindow()
         {
             InitializeComponent();
+            s ='a';
+        }
+
+        private void Button_Game_Start_Click(object sender, RoutedEventArgs e)
+        {
+            TextBlock_Word_1.Text = s.ToString();
+
+
+        }
+        private void Keyboard_Click(object sender, KeyEventArgs e)
+        {
+            char d = (char)e.Key;
+            TextBlock_Word_1.Text=d.ToString();
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            char d = (char)e.Key;
+            if (e.Key == Key.A)
+            {
+                TextBlock_Word_1.Text = d.ToString();
+            }
+            else if (e.Key == Key.B)
+            {
+                TextBlock_Word_1.Text = d.ToString();
+            }
+
+            // Обработка других клавиш
+        }
+
+        private void TextBlock_Word_2_KeyDown(object sender, KeyEventArgs e)
+        {
+            char d = (char)e.Key;
+            TextBlock_Word_1.Text = d.ToString();
         }
     }
 }
